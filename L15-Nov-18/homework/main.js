0a3633b0e93859c6e1e6214972bc7844e97a1db7
kaloy <kaloyan.baltiev@gmail.com>
2019-11-24 19:53:14 +0200

    homework done

---
+++ L15-Nov-18/homework/main.js
@@ -0,0 +1,50 @@
+// Task 1
+
+var array = [2, 1, 16, 3, 9];
+//		sum(array)      => 31
+//		sumEven(array)  => 18
+//		sumOdd(array)   => 13
+        
+function sum(array) {
+    let sum = 0;
+    array.forEach(function(element) {
+        sum += element;
+    });
+    return sum;
+};
+
+function sumEven(array) {
+    let sum = 0;
+    array.forEach(function(element) {
+        if(element % 2 == 0) {
+            sum += element;
+        }
+    });
+    return sum;
+}
+
+function sumOdd(array) {
+    let sum = 0;
+    array.forEach(function(element) {
+        if(element % 2 != 0) {
+            sum += element;
+        }
+    });
+    return sum;
+}
+// Task 2
+
+var array = ['dog', 'cat', 'mouse', 'lizard', 'bird'];
+//		first(array)     => 'dog'
+//      first(array, 3)  => ['dog', 'cat', 'mouse']
+        
+function first(array, n){
+    if(array.length == 0){
+        return 0;
+    }
+    if(n == null) {
+        return array[0];
+    } else{
+        return array.slice(0, n);
+    }
+};       
