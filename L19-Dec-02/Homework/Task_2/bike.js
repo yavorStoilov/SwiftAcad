8f1f2d1360055ac3b8382e265f99afc89263e873
MLapatova <marieta.lapatova@abv.bg>
2019-12-04 20:07:44 +0200

    homework

---
+++ L19-Dec-02/Homework/Task_2/bike.js
@@ -0,0 +1,12 @@
+class Bike {
+    constructor (brand, price, color, weight, bikeStyle){
+      this.brand = brand;
+      this.price = price;
+      this.color = color;
+      this.weight = weight;
+      this.bikeStyle = bikeStyle;
+      
+    }
+    
+    
+}
---
+++ L19-Dec-02/Homework/task_1.js
@@ -0,0 +1,12 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+function displayEvenNumbers(numbers) {
+    var evenNumbers = 
+	var arr = numbers;
+    for (var i=0 ; i < arr.length ; i++) {
+        if (arr[i] % 2 == 0) {
+        	evenNumbers.push(arr[i]);
+		}
+    }
+	return evenNumbers;
+}
+displayEvenNumbers(numbers); 
