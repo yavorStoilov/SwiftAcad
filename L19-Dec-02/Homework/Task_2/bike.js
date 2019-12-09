552b3edf4749a47ca17348893b5588fc34e0496a
MLapatova <marieta.lapatova@abv.bg>
2019-12-09 12:39:26 +0200

    changes

--- L19-Dec-02/Homework/Task_2/bike.js
+++ L19-Dec-02/Homework/Task_2/bike.js
@@ -9,22 +9,7 @@
       
     }
 }
-var el_down = document.getElementById("GFG_DOWN"); 
 
-function toHtml(obj){
-  var string = ''; 
 
-  for(var prop in obj) { 
-    if(typeof obj[prop] == 'string') { 
-        string+= prop + ': ' + obj[prop]+'; </br>'; 
-    } 
-    else { 
-        string+= prop + ': { </br>' + print(obj[prop]) + '}'; 
-    } 
-    return string; 
-  } 
-  function gfg_Run() {  
-    el_down.innerHTML = toHtml(Bike); 
-  }
-}
+
 })();
--- L19-Dec-02/Homework/task_1.js
+++ L19-Dec-02/Homework/task_1.js
@@ -1,7 +1,6 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-function displayEvenNumbers(numbers) {
-    var evenNumbers = 
-	var arr = numbers;
+function displayEvenNumbers(arr) {
+    var evenNumbers = [];
     for (var i=0 ; i < arr.length ; i++) {
         if (arr[i] % 2 == 0) {
         	evenNumbers.push(arr[i]);
