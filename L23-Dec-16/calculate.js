efff23c2b2d304fd419feffb29748e6921d41210
MLapatova <marieta.lapatova@abv.bg>
2019-12-16 21:19:20 +0200

    changes

---
+++ L23-Dec-16/calculate.js
@@ -0,0 +1,19 @@
+
+var calculator = document.calculator;
+var flag = false;
+function getResult() {
+    calculator.display.value = eval(calculator.display.value);
+    flag = true;
+};
+function clearResult() {
+    calculator.display.value = '';
+    flag = false;
+};
+function addNumber(val) {
+    if (flag) clearResult();
+    calculator.display.value += val;
+};
+function addSymbol(val) {
+    if (flag) flag = false;
+    calculator.display.value += val;
+};
